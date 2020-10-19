d1a37bc85f4b2e3d4fbf5a30b9af2265a8d1f959
sourceKing <miraclef60@gmail.com>
2020-10-19 03:53:13 +0100

    Refactored store page

--- pages/store.js
+++ pages/store.js
@@ -1,12 +1,106 @@
-import { useState, useEffect } from 'react';
-import BaseLayout from 'components/LayoutHOC';
+import { useState, useEffect } from "react";
+import Row from "antd/lib/row";
+import Col from "antd/lib/col";
+import Card from "antd/lib/card";
+import Image from 'antd/lib/image';
+import Divider from 'antd/lib/divider';
+import {} from "@ant-design/icons";
+import BaseLayout from "components/LayoutHOC";
 
 function Store() {
   return (
     <>
-      <h1>This is the stores page</h1>
+      <div
+        style={{
+          width: "100%",
+          padding: "auto 10px",
+          position: "relative",
+          display: "inline-block",
+        }}
+      >
+        <div
+          style={{
+            // width: "95%",
+            borderRadius: 10,
+            height: 280,
+            background: "#E2BE00",
+            display: "inline-block",
+            position: "absolute",
+            top: 0,
+            left: 10,
+            right: 10,
+            margin: "0 auto",
+          }}
+        ></div>
+        <Row
+          gutter={0}
+          style={{
+            margin: 0,
+            padding: 0,
+            paddingBottom: 90,
+            zIndex: 10,
+            position: "relative",
+          }}
+        >
+          <Col span={2} style={{ margin: 0, padding: 0 }}></Col>
+          <Col span={20} style={{ margin: 0, padding: 0 }}>
+            <Row
+              gutter={[64, 64]}
+              style={{ margin: 0, padding: 0, paddingTop: 90 }}
+            >
+              <Col span={8} style={{ margin: 0 }}>
+                <Card
+                  // title="Product title"
+                  style={{
+                    // background:
+                    //   "radial-gradient(rgba(225,225,225,.5) 20%, white)",
+                    backgroundImage: "url('/prod-img.jpg')",
+                    backgroundSize: '90%',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    height: 320,
+                    borderRadius: 30,
+                    textAlign: 'center',
+                  }}
+                >
+                  {/* <Image src="/prod-img.jpg" height={230} width={'100%'} /> */}
+                  <Divider orientation="center" type="horizontal">Popular dress amy</Divider>
+                  <Card.Meta description="$236.00" />
+                </Card>
+              </Col>
+              <Col span={8} style={{ margin: 0 }}>
+                <Card
+                  title="Product title"
+                  style={{
+                    background:
+                      "radial-gradient(rgba(225,225,225,.5) 20%, white)",
+                    height: 320,
+                    borderRadius: 10,
+                  }}
+                >
+                  <h1>Making a lot of mistakes in time</h1>
+                </Card>
+              </Col>
+              <Col span={8} style={{ margin: 0 }}>
+                <Card
+                  title="Product title"
+                  style={{
+                    background:
+                      "radial-gradient(rgba(225,225,225,.5) 20%, white)",
+                    height: 320,
+                    borderRadius: 10,
+                  }}
+                >
+                  <h1>Making a lot of mistakes in time</h1>
+                </Card>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={2} style={{ margin: 0, padding: 0 }}></Col>
+        </Row>
+      </div>
     </>
-  )
+  );
 }
 
 export default BaseLayout(Store);
